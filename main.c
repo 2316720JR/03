4fb4a0a1eeead2413cc70ee4657234ed2eb5464a
2316720JR <ljr2303@sookmyung.ac.kr>
2023-09-21 16:38:20 +0900

    Update main.c (3)

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
-	int i;
+	int a;
+	int b; 
 	
-	printf("enter a character :");
-	scanf("%c", &c);
+	printf("input numerator: ");
+	scanf("%d", &a);
 	
-	printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+	printf("input denominator: ");
+	scanf("%d", &b);
+	
+	printf("Devide result is %f \n", (float)a/b);
 	
 
 	return 0;
